fa03501a74057d7399014f4ebc070f5dd10f0669
William Zhang <WilliamHYZhang@gmail.com>
2020-01-26 15:58:04 -0500

    doctests

--- Sources/S5TF/utils/S5TFUtils.swift
+++ Sources/S5TF/utils/S5TFUtils.swift
@@ -20,7 +20,7 @@ public struct S5TFUtils {
     ///     shell(URL(string: "/bin/ls")!, "-lah")
     ///     ```
     @discardableResult
-    static public func shell(_ executableURL: URL, _ arguments: String...) throws -> (out: String?, status: Int32) {
+    static public func shell(_ executableURL: URL, _ arguments: [String]) throws -> (out: String?, status: Int32) {
         let task = Process()
         task.executableURL = executableURL
         task.arguments = arguments
@@ -39,24 +39,24 @@ public struct S5TFUtils {
     /// Extract a downloaded archive
     /// 
     /// - Parameters:
-    ///   - `archiveURL`: the URL for the archive.
+    ///   - `fileAtURL`: the URL for the archive.
     ///
     /// - Returns: output, termination status of the command.
     ///
     /// - Usage Example:
     ///   - Extract "archive.tgz":
     ///     ```
-    ///     extract(archiveURL: URL(string: "archive.tgz")!, fileExtension: "tgz")
+    ///     extract(fileAt: URL(string: "archive.tgz")!)
     ///     ```
     ///   - Extract "another_archive.gz"
     ///     ```
-    ///     extract(archiveURL: URL(string: "archive_archive.gz")!, fileExtension: "gz")
+    ///     extract(fileAt: URL(string: "archive_archive.gz")!)
     ///     ```
     ///
     @discardableResult
-    static public func extract(archiveURL: URL, fileExtension: String) throws -> (out: String?, status: Int32) {
-        
-        let path = archiveURL.path
+    static public func extract(fileAt: URL) throws -> (out: String?, status: Int32) {
+        let path = fileAt.path
+        let fileExtension = fileAt.pathExtension
 
         #if os(macOS)
             let binary = "/usr/bin/"
@@ -78,23 +78,27 @@ public struct S5TFUtils {
                 fatalError("Unsupported file extension for archive.")
         }
 
-        let task = Process()
-        task.executableURL = URL(fileURLWithPath: binary+tool)
-        task.arguments = arguments
-
-        let pipe = Pipe()
-        task.standardOutput = pipe
-        try task.run()
-        task.waitUntilExit()
-
-        let data = pipe.fileHandleForReading.readDataToEndOfFile()
-        let output = String(data: data, encoding: String.Encoding.utf8)
-
-        return (output, task.terminationStatus)
+        return try shell(URL(fileURLWithPath: binary+tool), arguments)
     }
 
+    /// Download and extract an archive.
+    /// 
+    /// - Parameters:
+    ///   - `fileAt`: the remote URL for the archive.
+    ///   - `cacheName`: name of the cache.
+    ///   - `fileName`: name of the file.
+    ///
+    /// - Returns: output, termination status of the command.
+    ///
+    /// - Usage Example:
+    ///   - Download and extract "https://storage.googleapis.com/cvdf-datasets/mnist/train-images-idx3-ubyte.gz":
+    ///     ```
+    ///     downloadAndExtract(fileAt: URL(string: "https://storage.googleapis.com/cvdf-datasets/mnist/train-images-idx3-ubyte.gz")!,
+    ///                        cacheName: "mnist", fileName: "train_images"))
+    ///     ```
+    ///
     @discardableResult
-    static public func downloadAndExtract(fileAt: URL, cacheName: String, fileName: String, fileExtension: String) throws -> (out: String?, status: Int32) {
+    static public func downloadAndExtract(fileAt: URL, cacheName: String, fileName: String) throws -> (out: String?, status: Int32) {
         let semaphore = DispatchSemaphore(value: 0)
         var archiveURL: URL? = nil
         let downloader = Downloader()
@@ -107,10 +111,6 @@ public struct S5TFUtils {
         }
         semaphore.wait()
 
-        do{
-            return try extract(archiveURL: archiveURL!, fileExtension: fileExtension)
-        } catch {
-            fatalError("Unable to extract file.")
-        }
+        return try extract(fileAt: archiveURL!)
     }
 }
